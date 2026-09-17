98fcbb41b5cb3f42beb9c7da21669b92fa37afcf
balochkhan788 <nadeen89144@gmail.com>
2026-09-17 21:45:42 +0100

    Fix service worker

--- public/pwabuilder-sw.js
+++ public/pwabuilder-sw.js
@@ -7,7 +7,3 @@ self.addEventListener("install", (event) => {
 self.addEventListener("activate", (event) => {
   event.waitUntil(self.clients.claim());
 });
-
-self.addEventListener("fetch", (event) => {
-  // Normal network requests
-});
